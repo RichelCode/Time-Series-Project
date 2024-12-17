21e406ff7548bc99ef3fb0a7b474478c0c610622
Mahsa Ashouri <mah.ashouri62@gmail.com>
2024-12-17 10:10:39 -0500

    Added fare column

--- arrival_series.R
+++ arrival_series.R
@@ -7,7 +7,8 @@ airport_traffic <- read_csv("usa.csv")
 
 airport_traffic_summary <- airport_traffic %>%
   group_by(airport_2, Year, quarter) %>%
-  summarise(total_passengers = sum(passengers, na.rm = TRUE), .groups = "drop")
+  summarise(total_passengers = sum(passengers, na.rm = TRUE),
+           total_fare = sum(fare, na.rm = TRUE), .groups = "drop" )
 
 airport_traffic_summary <- airport_traffic_summary %>%
   mutate(time_period = paste(Year, "Q", quarter, sep = "-"))
@@ -20,5 +21,8 @@ all_combinations <- airline_traffic %>%
 airport_traffic_filled <- all_combinations %>%
   left_join(airport_traffic_summary, by = c("airport_2", "Year", "quarter")) %>%
   mutate(total_passengers = ifelse(is.na(total_passengers), 0, total_passengers)) %>%
+  mutate(total_fare = ifelse(is.na(total_fare), 0, total_fare)) %>%
   mutate(time_period = paste(Year, "Q", quarter, sep = "-"))
+
+
 
